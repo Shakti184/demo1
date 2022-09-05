0a3d1f7e93c6c3f65f04a0d38c0eac40a451bb97
Shakti184 <shaktimaddeshiya184@gmail.com>
2022-09-06 00:28:48 +0530

    widget2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:demo1/widgit01.dart';
+import 'package:demo1/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,57 +16,6 @@ class MyApp extends StatelessWidget {
        primarySwatch: Colors.blueGrey,
       ),
       home: const MyHomePage(title: 'DEMO001'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-      appBar: AppBar(
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-       
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            const Widget01(),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
